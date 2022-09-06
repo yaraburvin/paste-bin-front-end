e439cd5e6a7eaf885e64fc75d7283430687995f7
yarab <yaraburvin@gmail.com>
2022-09-06 13:12:52 +0300

    Window for pastes submissions,content is limited to displaying 5 lines

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { IPaste } from "./utils/types";
 import { SinglePaste } from "./SinglePaste";
+import { PostingWindow } from "./PostingWindow";
+
+export const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://paste-bin-yara-burvin.herokuapp.com/"
+    : "http://localhost:4000";
 
 function App(): JSX.Element {
-  const baseUrl =
-    process.env.NODE_ENV === "production"
-      ? "https://paste-bin-yara-burvin.herokuapp.com/"
-      : "http://localhost:4000";
 
   const [allPastes, setAllPastes] = useState<IPaste[]>([]);
   useEffect(() => {
@@ -20,9 +22,12 @@ function App(): JSX.Element {
     } catch (err) {
       console.error(err);
     }
-  }, [allPastes, baseUrl]);
+  }, [allPastes]);
   return (
     <>
+      <section className="submission-field">
+        <PostingWindow allPastes={allPastes} setAllPastes={setAllPastes}/>
+      </section>
       <p>The app is running! Connceted to heroku.</p>
       {allPastes.map((el) => (
         <SinglePaste element={el} key={el.id} />
---
+++ src/PostingWindow.tsx
@@ -0,0 +1,37 @@
+import axios from "axios"
+import { useState } from "react"
+import { baseUrl } from "./App"
+import { IPaste } from "./utils/types"
+
+
+
+interface Iprops {
+    allPastes : IPaste[],
+    setAllPastes : React.Dispatch<React.SetStateAction<IPaste[]>>
+}
+export function PostingWindow ({allPastes, setAllPastes} :Iprops) : JSX.Element{
+    const [title, setTitle] = useState<string>('')
+    const [content, setContent] = useState<string>("")
+    const handleSubmit = async () => {
+        try {
+            const response : IPaste = await axios.post(baseUrl + "/pastes/", {
+                title : title,
+                content : content
+            })
+            console.log(response)
+            setAllPastes([response,...allPastes])
+            setTitle("")
+            setContent("")
+        }
+        catch (error) {
+            console.error(error)
+        }
+    }
+    return (
+        <div>
+            <input placeholder = "Title" type="text" value = {title} onChange={(e) => setTitle(e.target.value)}/>
+            <textarea className="input-box" value={content} onChange = {(e) => setContent(e.target.value)} placeholder = "Paste your text here."> </textarea>
+            <button onClick={() => handleSubmit()}>Submit</button>
+        </div>
+    )
+}
